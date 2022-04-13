ff2b216adcdcf0a7d5a309636d3152c30a2e6791
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-13 17:56:24 -0500

    Ejercicios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -159,12 +159,12 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero >= 1) {
-     console.log("Es positivo");
+  if (numero > 0) {
+      console.log("Es positivo");
   } else if (numero > 0){
-     console.log("Es negativo");
+      console.log("Es negativo");
   } else {
-    return false;
+      console.log("false");
   }
 }
 
