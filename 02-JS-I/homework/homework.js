5ab3e1f07aaef24475627273d3366be4dfd1bbfb
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-13 18:14:06 -0500

    Ejercicios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -181,7 +181,8 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var NombreYApellido = nombre + apellido
+  return NombreYApellido
 }
 
 function obtenerSaludo(nombre) {
