a7e22acb9c3ed30e6b829ab1f7215b6b609ef659
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-13 18:13:06 -0500

    Ejercicios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -172,8 +172,10 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return (str,"!")
+  var excla = str + '!'
+  return excla
 }
+  
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
