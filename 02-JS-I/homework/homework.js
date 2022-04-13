f94198082823c42763f7c486b13b7b82f657e155
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-13 18:44:47 -0500

    Ejercicios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -234,7 +234,11 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+    if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+    console.log('Es vocal')
+   } else if (letra.length !== 1) {
+   console.log('Dato incorrecto')
+   }
 }
 
 
