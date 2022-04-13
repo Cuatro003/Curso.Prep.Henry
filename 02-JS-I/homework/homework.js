cce931bd337f4f885f24e7480bc83a27f5d59a02
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-13 16:31:10 -0500

    Ejercicios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -121,7 +121,7 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  console.log(Math.pow(num, 2));
 }
 
 function elevarAlCubo(num) {
