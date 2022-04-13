aa5ee5aa36ef9d42a48aac05eec455edef51f709
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-13 18:20:37 -0500

    Ejercicios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -189,7 +189,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var HolaYAlgo = nombre + 'Martin!'
+  var HolaYAlgo = 'Hola' + nombre
   return HolaYAlgo
 }
 
