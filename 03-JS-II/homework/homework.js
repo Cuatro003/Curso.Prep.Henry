f483776fc3f6dda7761230b98e2b058e1ba7bf78
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-14 14:20:19 -0500

    Ejercicio Colors

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -133,7 +133,7 @@ function fizzBuzz(numero) {
     return 'fizz';
   } else if ( numero % 5 === 0){
     return 'buzz' 
-  } else if (numero % 3 && numero % 5){
+  } else if (numero % 3 && numero % 5 === 0){
     return 'fizzbuzz'
   } else {
     return numero;
