ca22d76c5d1571bf89aaccbf6d031ce9949221fe
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-14 15:49:34 -0500

    Ejercicio Colors

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -194,11 +194,7 @@ function tablaDelSeis(seis){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   var array = [];
-  for(var i = 0 ; 1 < 11 ; i++){
-    i * 7
-  }
-  return false;
-}
+  
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
