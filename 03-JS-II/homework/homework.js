9d304beab879242bd1d719fa8cd83e325d1ec8ed
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-14 16:44:22 -0500

    Ejercicio Colors

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -203,7 +203,7 @@ function tablaDelSeis(seis){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if((numero.toString()).lenth === 3){
+  if(numero > 99 && numero < 1000){
     return true;
   }
   return false;
