c7f23267fd58bc02d3ba3e614c6f87a979499760
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-14 15:31:53 -0500

    Ejercicio Colors

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -182,7 +182,7 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if (valor === 'true'){
+  if (valor === true){
     return 'Soy verdadero';
   } else{
     return 'Soy falso'
