cf8c31de0beab0514e9affa013f4c56931dc5545
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-18 13:46:57 -0500

    Realizando la tarea

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -99,7 +99,7 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-  var maximo = Math.max(numeros)
+  var maximo = Math.max.apply(null, numeros);
   return maximo;
 }
 
