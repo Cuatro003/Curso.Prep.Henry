2377cc5d324fed7e45ab458a4f1d1d7bc2fc741e
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-18 11:09:47 -0500

    Realizando la tarea

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -67,7 +67,7 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-  console.info(array.includes(elemento));
+  return array.includes(elemento);
 }
 
 
