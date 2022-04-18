e29534a9fdf4b7f6a52b08ebf03ba38b93e411cb
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-18 16:45:30 -0500

    Haciendo tarea

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,25 +1,20 @@
 // No cambies los nombres de las funciones.
 
-const { suma } = require("../../02-JS-I/homework/homework");
-
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
-  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
-  return array[array.length - 1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
-  return (array.length);
 }
 
 
@@ -28,11 +23,6 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
-  var arrayIncrementado = [];
-  for (let i = 0; i < array.length; i++){
-    arrayIncrementado.push(array[i]+1)
-  }
-  return arrayIncrementado;
 }
 
 
@@ -40,8 +30,6 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  array.push(elemento);
-  return array;
 }
 
 
@@ -50,8 +38,6 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
-  array.unshift(elemento);
-  return array;
 }
 
 
@@ -61,7 +47,6 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-  return palabras.join(' ')
 }
 
 
@@ -69,7 +54,6 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-  return array.includes(elemento);
 }
 
 
@@ -77,21 +61,13 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
-  var acumulador = 0;
-  for(var i = 0; i < numeros.length ; i++){
-    acumulador += numeros[i]
-  }
-  return acumulador;
-  }
+}
 
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  let suma = resultadosTest.reduce((previous, current) => current += previous);
-  let promedio = suma / resultadosTest.length;
-  return promedio;
 }
 
 
@@ -99,54 +75,29 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-  var maximo = Math.max.apply(null, numeros);
-  return maximo;
 }
 
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
+  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-  var acuMulti = 1;
-  if(arguments.lengt === 0){
-    return 0;
-  } else if(arguments.length === 1){
-    return arguments[0];
-  }else{
-    for (var i = 0; i < arguments.length ; i++){
-         acuMulti = acuMulti * arguments[i]
-    }
-  }
-  return acuMulti;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-  var acumulador = 0;
-  for (var i = 0; i < arreglo.lengt ; i++){
-    if (arreglo[i] > 18){
-    acumulador += 1
-    }
-  }
-  return acumulador;
+
 }
 
 
 function diaDeLaSemana(numeroDeDia) {
-  // domingo 1| lunes 2| martes 3| miercoles 4| jueves 5| viernes 6| sabado 7|
-  //
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  if(numeroDeDia === 1 || numeroDeDia === 7){
-    return 'Es fin de semana'
-  }else if(numeroDeDia >1 && numeroDeDia < 7){
-    return 'Es dia laboral';
-  }
+  
 } 
 
 
