05e8877bf04ac1f923883ebd88911ea81bf5b8ba
Tobias <tobiaslautaro.maidanabernardi@gmail.com>
2022-04-22 18:40:09 -0300

    Finalizada

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -153,10 +153,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-  producto.calcularPrecioDescuento = function() {
-    return this.precio - ( this.precio * this.porcentajeDeDescuento );
-  };
-  return producto;
+  producto.calcularPrecioDescuento = function(){
+    return this.precio - (this.precio * this.calcularPrecioDescuento)
+  }
+  return producto
 }
 
 // No modificar nada debajo de esta línea
